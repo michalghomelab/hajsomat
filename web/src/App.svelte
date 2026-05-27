0019370fec387e5c813921a7a43ad0accad307df
michal <michal@gilewski.it>
2026-05-27 11:21:48 +0200

    feat: hash-based routing so portfolio view survives page refresh

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,11 +1,28 @@
 <script>
   import PortfolioList from "./lib/components/PortfolioList.svelte";
   import PortfolioView from "./lib/components/PortfolioView.svelte";
-  let selectedId = $state(null);
+
+  function parseHash() {
+    const m = location.hash.match(/^#\/portfolio\/(\d+)/);
+    return m ? Number(m[1]) : null;
+  }
+
+  let selectedId = $state(parseHash());
+
+  function open(id) { location.hash = `#/portfolio/${id}`; }
+  function back() { location.hash = "#/"; }
+
+  $effect(() => {
+    const onHash = () => { selectedId = parseHash(); };
+    window.addEventListener("hashchange", onHash);
+    return () => window.removeEventListener("hashchange", onHash);
+  });
 </script>
 
 {#if selectedId}
-  <PortfolioView id={selectedId} onBack={() => (selectedId = null)} />
+  {#key selectedId}
+    <PortfolioView id={selectedId} onBack={back} />
+  {/key}
 {:else}
-  <PortfolioList onSelect={(id) => (selectedId = id)} />
+  <PortfolioList onSelect={open} />
 {/if}
